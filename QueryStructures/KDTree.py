1a5ebe8d176d2f9a0df38817170a3a39c1eebce0
ntmdrgl <129223678+ntmdrgl@users.noreply.github.com>
2023-11-12 23:15:55 -0800

    update

---
+++ QueryStructures/KDTree.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on: November, 2023
+Authors: Nathaniel Madrigal, Alexander Madrigal
+"""
+
+import numpy as np
+
+class KDTree():
+    def __init__(self):
+        print('kdtree')
+
